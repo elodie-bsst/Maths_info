f57307d1cc4a3de9219fc6ff2fac346a1fb67f8a
elodie-bsst <73130147+elodie-bsst@users.noreply.github.com>
2021-03-28 19:59:02 +0200

    Add files via upload
    ajout des classes de base

---
+++ Automate.java
@@ -0,0 +1,11 @@
+package package_1;
+
+public class Automate {
+	private Etat []etat;
+	private Transition []transition;
+	private int nombreEtat;
+	private int nombreTransitions;
+
+		
+
+}
---
+++ Etat.java
@@ -0,0 +1,6 @@
+package package_1;
+
+public class Etat {
+	private int numero;
+
+}
