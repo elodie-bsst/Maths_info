fd0f072adcae8ec467c257b0c104f9156525a763
Chloerlld123 <73694451+Chloerlld123@users.noreply.github.com>
2021-04-26 17:33:08 +0200

    Changes

--- Etat.java
+++
@@ -1,8 +0,0 @@
-package package_1;
-
-public class Etat {
-	private int numero;
-	private boolean initial ; 
-	private boolean terminal ; 
-
-}
--- Transition.java
+++
@@ -1,7 +0,0 @@
-package package_1;
-
-public class Transition {
-	private char symbole; 
-	private Etat etat_depart ; 
-	private Etat etat_arrivee ; 
-}
